47fc9dfd1e664dd033d015caf8304575637729c7
Alicia Hotovec-Ellis <ahotovec-ellis@usgs.gov>
2023-02-14 11:03:48 -0800

    Fixed input to create_family_images()

--- forcePlot.py
+++ forcePlot.py
@@ -102,7 +102,7 @@ if args.famplot:
     # Get correlation matrix and ids
     ids, ccc_sparse = redpy.correlation.get_matrix(rtable, ctable, opt)
     redpy.plotting.create_family_images(rtable, ftable, rtimes, rtimes_mpl,
-                              windowAmps[:,opt.printsta], ids, ccc_sparse, opt)
+                              windowAmps, ids, ccc_sparse, opt)
 if args.html:
     if opt.checkComCat==True:
         ttimes = ttable.cols.startTimeMPL[:] + opt.ptrig/opt.samprate/86400
