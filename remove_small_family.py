c5d7b4b87bc1c8d22ec55675a04e080551514ea7
Alicia Hotovec-Ellis <ahotovec-ellis@usgs.gov>
2023-03-17 10:59:00 -0700

    Refactored; renamed several options/variables

---
+++ remove_small_family.py
@@ -0,0 +1,130 @@
+# REDPy - Repeating Earthquake Detector in Python
+# Copyright (C) 2016-2020  Alicia Hotovec-Ellis (ahotovec-ellis@usgs.gov)
+# Licensed under GNU GPLv3 (see LICENSE.txt)
+"""
+Remove "small" families with few members.
+
+Run this script to remove small families (i.e., families that have less than
+MINMEMBERS members and are more than MAXAGE days old). Reclusters and
+remakes images when done. Optionally, the list of families that meet those
+criteria may be printed to screen without removing them.
+
+Note: Removing families from large datasets may take a significant amount of
+time.
+
+usage: remove_small_family.py [-h] [-v] [-c CONFIGFILE] [-m MINMEMBERS]
+                              [-a MAXAGE] [-t SEEDTIME] [-l]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         increase written print statements
+  -c CONFIGFILE, --configfile CONFIGFILE
+                        use configuration file named CONFIGFILE instead of
+                        default settings.cfg
+  -m MINMEMBERS, --minmembers MINMEMBERS
+                        minimum family size to keep; 5 by default
+  -a MAXAGE, --maxage MAXAGE
+                        maximum age of a family to be saved in days; 0 by
+                        default removes every small family that began before
+                        SEEDTIME
+  -t SEEDTIME, --seedtime SEEDTIME
+                        time from which to compute family age times (yyyy-
+                        mm-dd or yyyy-mm-ddTHH:MM:SS); last trigger time by
+                        default
+  -l, --listonly        list families to keep and to remove, but do not
+                        execute removal
+"""
+import argparse
+
+import matplotlib.dates as mdates
+from obspy import UTCDateTime
+
+import redpy
+
+
+def parse():
+    """
+    Define and parse acceptable command line inputs.
+
+    Returns
+    -------
+    args : ArgumentParser object
+
+    """
+    parser = argparse.ArgumentParser(
+        description='Remove "small" families with few members.')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='increase written print statements')
+    parser.add_argument('-c', '--configfile', default='settings.cfg',
+                        help=('use configuration file named CONFIGFILE '
+                              'instead of default settings.cfg'))
+    parser.add_argument('-m', '--minmembers', type=int, default=5,
+                        help='minimum family size to keep; 5 by default')
+    parser.add_argument('-a', '--maxage', type=float, default=0,
+                        help=('maximum age of a family to be saved in days; '
+                              '0 by default removes every small family '
+                              'that began before SEEDTIME'))
+    parser.add_argument('-t', '--seedtime', default='',
+                        help=('time from which to compute family age '
+                              'times (yyyy-mm-dd or yyyy-mm-ddTHH:MM:SS); '
+                              'last trigger time by default'))
+    parser.add_argument('-l', '--listonly', action='store_true', default=False,
+                        help=('list families to keep and to remove, but do '
+                              'not execute removal'))
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """Handle run from the command line."""
+    args = parse()
+    remove_small_family(**vars(args))
+    print('Done')
+
+
+def remove_small_family(configfile='settings.cfg', minmembers=5, maxage=0,
+                        seedtime='', listonly=False, verbose=False):
+    """
+    Remove "small" families with few members.
+
+    Parameters
+    ----------
+    configfile : str, optional
+        Configuration file to read.
+    minmembers : int, optional
+        Minimum number of family members in order to be kept; 5 by default.
+    maxage : int, optional
+        Maximum age of a family to be saved in days, calculated as the
+        difference between 'seedtime' and to the family start time. This
+        allows recent small families time to grow and removes truly "stale"
+        families. 0 by default, which removes all small families that
+        began before 'seedtime.'
+    seedtime : str, optional
+        Reference date for age calculation; defaults to last trigger time.
+    listonly : bool, optional
+        Only print a list of families that would be removed.
+    verbose : bool, optional
+        Enable additional print statements.
+
+    """
+    h5file, rtable, otable, ttable, ctable, _, dtable, ftable, opt = \
+        redpy.table.open_with_cfg(configfile, verbose)
+    if seedtime:
+        seedtime = UTCDateTime(seedtime)
+    else:
+        seedtime = UTCDateTime(mdates.num2date(ttable[-1]['startTimeMPL']))
+    removed_families = redpy.table.remove_small_families(
+        rtable, ctable, dtable, ftable, ttable, minmembers, maxage, seedtime,
+        opt, list_only=listonly)
+    if removed_families and not listonly:
+        redpy.plotting.generate_all_outputs(rtable, ftable, ttable, ctable,
+                                            otable, opt)
+        redpy.plotting.remove_old_files(ftable, opt)
+    else:
+        if opt.verbose:
+            print('No families removed. No plots to update...')
+    h5file.close()
+
+
+if __name__ == "__main__":
+    main()
