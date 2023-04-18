cb3a9e7d06399a54d8d217d7d5fa364312b5b2fc
Alicia Hotovec-Ellis <ahotovec-ellis@usgs.gov>
2023-04-18 16:20:46 -0700

    Fixed pylint complaint

--- redpy/outputs/mapping.py
+++ redpy/outputs/mapping.py
@@ -35,6 +35,10 @@ def create_local_map(detector, fnum, local):
 
     """
     if len(local['deps']) > 0:
+        # pylint: disable=E0110
+        # Abstract class instantiation from cartopy
+        plate_carree = ccrs.PlateCarree()
+        # pylint: enable=E0110
         stamen_terrain = cimgt.StamenTerrain()
         extent = [np.median(local['lons']) - detector.get('locdeg')/2,
                   np.median(local['lons']) + detector.get('locdeg')/2,
@@ -42,33 +46,33 @@ def create_local_map(detector, fnum, local):
                   np.median(local['lats']) + detector.get('locdeg')/4]
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, projection=stamen_terrain.crs)
-        ax.set_extent(extent, crs=ccrs.PlateCarree())
+        ax.set_extent(extent, crs=plate_carree)
         ax.add_image(stamen_terrain, 11)
         ax.set_xticks(np.arange(np.floor(10*(extent[0]))/10,
-                      np.ceil(10*(extent[1]))/10, 0.1), crs=ccrs.PlateCarree())
+                      np.ceil(10*(extent[1]))/10, 0.1), crs=plate_carree)
         ax.set_yticks(np.arange(np.floor(10*(extent[2]))/10,
-                      np.ceil(10*(extent[3]))/10, 0.1), crs=ccrs.PlateCarree())
-        ax.set_extent(extent, crs=ccrs.PlateCarree())  # Reset to fill image
+                      np.ceil(10*(extent[3]))/10, 0.1), crs=plate_carree)
+        ax.set_extent(extent, crs=plate_carree)  # Reset to fill image
         ax.xaxis.set_major_formatter(LongitudeFormatter())
         ax.yaxis.set_major_formatter(LatitudeFormatter())
         plt.yticks(rotation=90, va='center')
         ax.scatter(local['lons'], local['lats'], s=20, marker='o',
-                   color='white', transform=ccrs.PlateCarree())
+                   color='white', transform=plate_carree)
         ax.scatter(local['lons'], local['lats'], s=5, marker='o',
-                   color='red', transform=ccrs.PlateCarree())
+                   color='red', transform=plate_carree)
         ax.scatter(detector.get('stalons'), detector.get('stalats'),
                    marker='^', color='k', facecolors='None',
-                   transform=ccrs.PlateCarree())
+                   transform=plate_carree)
         scalebar_lat = 0.05*(detector.get('locdeg')/2) + extent[2]
         scalebar_lon_left = 0.05*(detector.get('locdeg')) + extent[0]
         scalebar_len = kilometers2degrees(10) / np.cos(
             scalebar_lat*np.pi / 180)
         ax.plot((scalebar_lon_left, scalebar_lon_left + scalebar_len),
                 (scalebar_lat, scalebar_lat), 'k-',
-                transform=ccrs.PlateCarree(), lw=2)
+                transform=plate_carree, lw=2)
         # pylint: disable=W0212
         # I trust use of protected member here.
-        geodetic_transform = ccrs.PlateCarree()._as_mpl_transform(ax)
+        geodetic_transform = plate_carree._as_mpl_transform(ax)
         # pylint: enable=W0212
         text_transform = offset_copy(geodetic_transform, units='dots', y=5)
         ax.text(scalebar_lon_left + scalebar_len/2,
